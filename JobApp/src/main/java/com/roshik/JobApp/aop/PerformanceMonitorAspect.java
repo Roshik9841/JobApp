20fe52f37eb8ad3e0b91c8f4cc7af06c5034af6f
I am Roshik <Roshik9841@gmail.com>
2025-11-14 17:43:00 +0545

    performance monitoring using around

---
+++ JobApp/src/main/java/com/roshik/JobApp/aop/PerformanceMonitorAspect.java
@@ -0,0 +1,37 @@
+package com.roshik.JobApp.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class PerformanceMonitorAspect {
+
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PerformanceMonitorAspect.class);
+
+    @Around("execution(* com.roshik.JobApp.service.JobService.getJob(..))")
+    public Object MonitorTime(ProceedingJoinPoint jp) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        Object obj = jp.proceed();
+
+        long end = System.currentTimeMillis();
+
+        LOGGER.info("Time taken:" +(end-start));
+        return obj;
+    }
+
+
+
+
+
+}
+
