26e9695f4766b02f3b5117a3c1c6461598b30e2d
I am Roshik <Roshik9841@gmail.com>
2025-11-14 17:11:45 +0545

    After aspect

--- JobApp/src/main/java/com/roshik/JobApp/aop/LoggingAspect.java
+++ JobApp/src/main/java/com/roshik/JobApp/aop/LoggingAspect.java
@@ -1,8 +1,7 @@
 package com.roshik.JobApp.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -12,11 +11,32 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
-
+    //before chahi method call hunu aagadi call hunxa
     //return type, class name-method name(arguments)
-    @Before("execution(* com.roshik.JobApp.service.JobService.getJob(..))")
+    @Before("execution(* com.roshik.JobApp.service.JobService.getJob(..)) || execution(* com.roshik.JobApp.service.JobService.updateJob(..))")
     public void logMethodCall(JoinPoint jp){
 
         LOGGER.info("Method Called"+ jp.getSignature().getName());
+    }
+
+
+    //yo chahi method call bhaye call hunxa yesma error aayeni naayeni call hunxa yo
+    @After("execution(* com.roshik.JobApp.service.JobService.getJob(..)) || execution(* com.roshik.JobApp.service.JobService.updateJob(..))")
+    public void logMethodExecuted(JoinPoint jp){
+
+        LOGGER.info("Method Executed"+ jp.getSignature().getName());
+    }
+
+    //yo error aayo bhane call hunxa
+    @AfterThrowing("execution(* com.roshik.JobApp.service.JobService.getJob(..)) || execution(* com.roshik.JobApp.service.JobService.updateJob(..))")
+    public void logMethodCrash(JoinPoint jp){
+
+        LOGGER.info("Method has some issue"+ jp.getSignature().getName());
+    }
+    //yo method execute bhayesi mtra call hunxa
+    @AfterReturning("execution(* com.roshik.JobApp.service.JobService.getJob(..)) || execution(* com.roshik.JobApp.service.JobService.updateJob(..))")
+    public void logMethodExecutedSuccess(JoinPoint jp){
+
+        LOGGER.info("Method executed succesfully"+ jp.getSignature().getName());
     }
 }
